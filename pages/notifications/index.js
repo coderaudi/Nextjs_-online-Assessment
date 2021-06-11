57fea6fbb21cc410a0f48ad46e6de5f6b6d208e8
Abhijeet Khire <abhijeetkhire@gmail.com>
2021-06-11 13:24:44 +0530

    🆕 NEW: File base Routing

---
+++ pages/notifications/index.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const index = memo(() => {
+    return (
+        <div>
+            <h1>Notifcations</h1>
+        </div>
+    );
+});
+
+export default index;
