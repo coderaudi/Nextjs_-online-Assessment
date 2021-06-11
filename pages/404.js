cc14ef4d6439752083496bbe417b85a53a175e88
Abhijeet Khire <abhijeetkhire@gmail.com>
2021-06-11 14:39:57 +0530

    custome 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const notFoundPage = memo(() => {
+    return (
+        <div>
+            <h1>Not found page</h1>
+        </div>
+    );
+});
+
+export default notFoundPage;
